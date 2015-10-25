3313019e4f89a68fdc5ebfe9bef5e7ecea62c85a
Craig Lamont <craig@techiteasy.org.uk>
2015-10-25 20:16:26 +0000

    made a bit prettier

--- controllers/genres_controller.rb
+++ controllers/genres_controller.rb
@@ -29,27 +29,16 @@ end
   end
 
   post '/genres/:id/delete' do
-    
     remove_orphans(params[:id])
     Genre.find(params[:id]).destroy
-    
     redirect to('/genres')
   end
 
   def remove_orphans(id)
-    # get bookmarks with genre
-     @updated = []
-     orphan_bookmarks = Bookmark.find_by_genre(id)
-    # #change genre to blank
-    #  #<Bookmark:0x007fe110093e88 @details="BBC News front page", @genre=nil, @genre_id="4", @id="2", @title="BBC News", @url="http://www.bbc.co.uk/news">
-    # {"title"=>"BBC News", "url"=>"http://www.bbc.co.uk/news", "genre_id"=>"3"}
-     orphan_bookmarks.each { |orphan|  orphan.genre_id = "1" 
-      @bookmark = Bookmark.find(orphan.id)
-      @bookmark.update_attributes("genre_id"=>"1")
-    # #save changes to bookmarks database
-    # @bookmark = Bookmark.find(orphan.id)
-    # @updated << @bookmark
-  #   # @bookmark.update_attributes(params[:bookmarks])
+    orphan_bookmarks = Bookmark.find_by_genre(id)
+    orphan_bookmarks.each { |orphan|  orphan.genre_id = "1" 
+    @bookmark = Bookmark.find(orphan.id)
+    @bookmark.update_attributes("genre_id"=>"1")
    }
     
   end
